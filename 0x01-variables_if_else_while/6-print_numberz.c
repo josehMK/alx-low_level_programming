a792a18e771ad8716c770edaf2bee8fc18ce8ba5
josehMK <j.mburu.w7@gmail.com>
2023-07-15 02:45:05 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int r;
+
+	r = 0;
+	while
+		(r < 10) {
+			putchar(r + '0');
+			r++;
+		}
+	putchar('\n');
+	return (0);
+}
