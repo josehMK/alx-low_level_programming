9be81d9b7be3536a1132d23a0014ca30a6d751e0
josehMK <j.mburu.w7@gmail.com>
2023-07-18 11:43:10 -0700

    3-islower.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *print alphabet - make alphabet
- *
+ * print alphabet - make alphabet
+ * Description: making lowercase alphabet
  * Return: void
  */
 
 void print_alphabet(void)
 {
 	char A;
+
 	for (A = 'a'; A <= 'z'; A++)
 	_putchar(A);
 	_putchar('\n');
